95035b9963277f381b0e4cb825f3d36e0edb8c29
Scald <104459145+Arikatsu@users.noreply.github.com>
2025-02-23 15:13:35 +0530

    Instruction tests

--- src/CPU.cs
+++ src/CPU.cs
@@ -41,6 +41,82 @@ public class CPU
                 _registers[reg] += _registers[value];
                 break;
             
+            case 0x04: // SUB reg, imm
+                _registers[reg] -= value;
+                break;
+            
+            case 0x05: // SUB reg, reg
+                _registers[reg] -= _registers[value];
+                break;
+            
+            case 0x06: // MUL reg, imm
+                _registers[reg] *= value;
+                break;
+            
+            case 0x07: // MUL reg, reg
+                _registers[reg] *= _registers[value];
+                break;
+            
+            case 0x08: // DIV reg, imm
+                _registers[reg] /= value;
+                break;
+            
+            case 0x09: // DIV reg, reg
+                _registers[reg] /= _registers[value];
+                break;
+            
+            case 0x0A: // AND reg, imm
+                _registers[reg] &= value;
+                break;
+            
+            case 0x0B: // AND reg, reg
+                _registers[reg] &= _registers[value];
+                break;
+            
+            case 0x0C: // OR reg, imm
+                _registers[reg] |= value;
+                break;
+            
+            case 0x0D: // OR reg, reg
+                _registers[reg] |= _registers[value];
+                break;
+            
+            case 0x0E: // XOR reg, imm
+                _registers[reg] ^= value;
+                break;
+            
+            case 0x0F: // XOR reg, reg
+                _registers[reg] ^= _registers[value];
+                break;
+            
+            case 0x10: // NOT reg
+                _registers[reg] = ~_registers[reg];
+                break;
+            
+            case 0x11: // SHL reg, imm
+                _registers[reg] <<= (int)value;
+                break;
+            
+            case 0x12: // SHL reg, reg
+                _registers[reg] <<= (int)_registers[value];
+                break;
+            
+            case 0x13: // SHR reg, imm
+                _registers[reg] >>= (int)value;
+                break;
+            
+            case 0x14: // SHR reg, reg
+                _registers[reg] >>= (int)_registers[value];
+                break;
+            
+            case 0x15: // CMP reg, imm
+                _registers[reg] = _registers[reg] == value ? 1u : 0u;
+                break;
+            
+            case 0x16: // CMP reg, reg
+                _registers[reg] = _registers[reg] == _registers[value] ? 1u : 0u;
+                break;
+            
             case 0xFF: // HLT
                 _pc = Memory.Size;
                 break;
@@ -50,9 +126,10 @@ public class CPU
         }
     }
     
-    public void Run()
+    public void Run(uint maxCycles = 1000)
     {
-        while (_pc < Memory.Size)
+        uint cycles = 0;
+        while (_pc < Memory.Size && cycles++ < maxCycles)
         {
             Fetch();
         }
